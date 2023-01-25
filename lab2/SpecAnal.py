d62c856adc5596acca9f12cb31e8c652a8a8fe4b
Colin Braun <electrolitic21@gmail.com>
2023-01-25 01:15:21 -0500

    Finish part 1 of lab 2

--- lab2/SpecAnal.py
+++ lab2/SpecAnal.py
@@ -10,6 +10,35 @@ import numpy as np                 # Numpy is a library support computation of l
 from PIL import Image              # Python Imaging Library (abbreviated as PIL) is a free and open-source additional library for the Python programming language that adds support for opening, manipulating, and saving many different image file formats.
 import matplotlib.pyplot as plt    # Matplotlib is a plotting library for the Python programming language.
 
+def BetterSpecAnal(x):
+    y_center = int(len(x)/2)
+    x_center = int(len(x[0])/2)
+
+    # Size of blocks
+    block_size = 64
+    # Number of blocks
+    num_blocks = 25
+
+    # Create the Hamming window
+    hw = np.outer(np.hamming(block_size), np.hamming(block_size))
+    total_pwr_spec_density = np.zeros([block_size, block_size])
+    # windows = []
+    for i in (-2, -1, 0, 1, 2):
+        for j in (-2, -1, 0, 1, 2):
+            y_start = y_center - i*block_size
+            x_start = x_center - j*block_size
+            # Create the window
+            window = x[y_start:y_start+block_size, x_start:x_start+block_size]
+            # Multiply it by the hamming window
+            window = window * hw
+            # Compute squared DFT magnitude of window
+            window = (1/block_size**2)*np.abs(np.fft.fft2(window))**2
+            window = np.fft.fftshift(window)
+            total_pwr_spec_density += window / 25
+            # windows.append(window)
+    return total_pwr_spec_density
+
+
 # Read in a gray scale TIFF image.
 im = Image.open('img04g.tif')
 print('Read img04.tif.')
@@ -36,7 +65,7 @@ x = np.double(x)/255.0
 
 i = 99
 j = 99
-N =64
+N = 256
 
 z = x[i:N+i, j:N+j]
 
@@ -60,8 +89,30 @@ surf = ax.plot_surface(X, Y, Zabs, cmap=plt.cm.coolwarm)
 ax.set_xlabel('$\mu$ axis')
 ax.set_ylabel('$\\nu$ axis')
 ax.set_zlabel('Z Label')
+ax.set_title(f"{N}x{N} Power Spectral Density")
 
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
+fig.savefig(f"images/log-pwr-spec-density-{N}x{N}.png")
+
 plt.show()
 
+# Plot the result using a 3-D mesh plot and label the x and y axises properly. 
+N = 64
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+a = b = np.linspace(-np.pi, np.pi, num = N)
+X, Y = np.meshgrid(a, b)
+
+surf = ax.plot_surface(X, Y, np.log(BetterSpecAnal(x)), cmap=plt.cm.coolwarm)
+
+ax.set_xlabel('$\mu$ axis')
+ax.set_ylabel('$\\nu$ axis')
+ax.set_zlabel('Z Label')
+ax.set_title(f"Better Power Spectral Density")
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+fig.savefig(f"images/log-pwr-spec-density-better.png")
+
+plt.show()
