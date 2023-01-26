b699d9255ed807d72f181d2feba29748704e2b71
Colin Braun <electrolitic21@gmail.com>
2023-01-26 16:25:23 -0500

    Implement theoretical power spectral density for y, create and write out most of the submission document

--- lab2/main.py
+++ lab2/main.py
@@ -33,16 +33,22 @@ def filter(x):
                 
     return result
 
+def h(u, v):
+    """
+    Compute the transfer function of the IIR filter at the specified spatial frequency domain points.
+    """
+    values = 3 / (1 - 0.99*np.exp(-1j*u) - 0.99*np.exp(-1j*v) + 0.9801*np.exp(-1j*u)*np.exp(-1j*v))
+    return np.absolute(values)**2
 
 x = np.random.uniform(-0.5, 0.5, [512, 512])
 x_scaled = 255 * (x + 0.5)
-plt.imshow(x_scaled.astype(np.uint8))
+plt.imshow(x_scaled.astype(np.uint8), cmap=plt.cm.binary)
 print(x_scaled.dtype)
 plt.savefig("images/np-rand-uniform.png")
 # plt.show()
 plt.clf()
 y = filter(x) + 127
-plt.imshow(y.astype(np.uint8))
+plt.imshow(y.astype(np.uint8), cmap=plt.cm.binary)
 # print(filter(x) + 127)
 plt.savefig("images/image-p-127.png")
 # plt.show()
@@ -58,17 +64,17 @@ surf = ax.plot_surface(X, Y, Z, cmap=plt.cm.coolwarm)
 
 plt.savefig("images/y-log-pwr-spec-density-better.png")
 
-# figure = plt.figure()
-# axis = figure.add_subplot(111, projection='3d')
-# 
-# u = np.linspace(-pi, pi, 1000)
-# v = np.linspace(-pi, pi, 1000)
-# # data = np.random.random((1000, 1000))
-# U, V = np.meshgrid(u, v)
-# # ----------------SECTION 3 PLOTTING-----------------
-# z = np.array(func2(U.ravel(), V.ravel()))
-# Z = z.reshape(U.shape)
-# 
-# axis.plot_surface(U, V, Z)
-# plt.savefig("results/section3-python.png")
-# # plt.show()
+figure = plt.figure()
+axis = figure.add_subplot(111, projection='3d')
+
+u = np.linspace(-pi, pi, 1000)
+v = np.linspace(-pi, pi, 1000)
+U, V = np.meshgrid(u, v)
+# ----------------SECTION 3 PLOTTING-----------------
+z = np.array(h(U.ravel(), V.ravel()))
+Z = z.reshape(U.shape)
+
+axis.plot_surface(U, V, np.log(Z), cmap=plt.cm.coolwarm)
+# axis.set_zlim(-2.5, 10)
+plt.savefig("images/y-log-pwr-spec-density-theoretical.png")
+# plt.show()
