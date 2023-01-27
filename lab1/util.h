a1f373488181b0eb80f6c46fca6ac78805bc5faa
Colin Braun <electrolitic21@gmail.com>
2023-01-27 18:36:57 -0500

    Work on lab 3

--- lab1/util.h
+++ lab1/util.h
@@ -4,5 +4,8 @@
 double ***get_color_image(size_t width, size_t height);
 void free_color_image(void ***pt);
 void save_color_image_to_tiff(double ***img, size_t width, size_t height, char *filename);
+void save_grayscale_image_to_tiff(double **img, size_t width, size_t height, char *filename);
+// unsigned char type image of the above version of the above
+void save_grayscale_image_to_tiff_c(unsigned char **img, size_t width, size_t height, char *filename);
 double **convolve(double **img, size_t width, size_t height, double **h, size_t h_width, size_t h_height);
 double ***tiff_to_double(struct TIFF_img *tiff_img);
--- lab3/section1.c
+++ lab3/section1.c
@@ -5,78 +5,168 @@
 #include <randlib.h>
 #include <typeutil.h>
 
+typedef struct pixel
+{
+    int m, n; /* m=row, n=col */
+} Pixel;
+
 void error(char *name);
 
-int main (int argc, char **argv) 
+void ConnectedNeighbors(
+    Pixel s,
+    double T,
+    unsigned char **img,
+    int width,
+    int height,
+    int *M,
+    Pixel c[4]
+);
+
+void ConnectedSet(
+    Pixel s,
+    double T,
+    unsigned char **img,
+    int width,
+    int height,
+    int ClassLabel,
+    unsigned int **seg,
+    int *NumConPixels
+);
+
+int main(int argc, char **argv)
 {
-  FILE *fp;
-  struct TIFF_img input_img;
-  int32_t i,j;
+    FILE *fp;
+    struct TIFF_img input_img;
+    int32_t i, j;
 
-  if ( argc != 2 ) error( argv[0] );
+    // if (argc != 2)
+    //     error(argv[0]);
 
-  /* open image file */
-  if ( ( fp = fopen ( argv[1], "rb" ) ) == NULL ) {
-    fprintf ( stderr, "cannot open file %s\n", argv[1] );
-    exit ( 1 );
-  }
+    // /* open image file */
+    // if ((fp = fopen(argv[1], "rb")) == NULL)
+    // {
+    //     fprintf(stderr, "cannot open file %s\n", argv[1]);
+    //     exit(1);
+    // }
+    char *fileName = "img22gd2.tif";
+    if ((fp = fopen(fileName, "rb")) == NULL)
+    {
+        fprintf(stderr, "cannot open file %s\n", fileName);
+        exit(1);
+    }
 
-  /* read image */
-  if ( read_TIFF ( fp, &input_img ) ) {
-    fprintf ( stderr, "error reading file %s\n", argv[1] );
-    exit ( 1 );
-  }
+    /* read image */
+    if (read_TIFF(fp, &input_img))
+    {
+        fprintf(stderr, "error reading file %s\n", fileName);
+        exit(1);
+    }
 
-  /* close image file */
-  fclose ( fp );
+    /* close image file */
+    fclose(fp);
 
-  /* check the type of image data */
-  if ( input_img.TIFF_type != 'c' ) {
-    fprintf ( stderr, "error:  image must be 24-bit color\n" );
-    exit ( 1 );
-  }
+    /* check the type of image data */
+    char imageType = 'g';
+    if (input_img.TIFF_type != imageType)
+    {
+        fprintf(stderr, "error:  Wrong image type\n");
+        exit(1);
+    }
 
-  //----------------------SMOOOTHED IMAGE---------------------------
-  // Create the image we want
-  struct TIFF_img smoothed_image;
-  get_TIFF ( &smoothed_image, input_img.height, input_img.width, 'c' );
-  /* Filter image along horizontal direction */
-  for ( i = 4; i < input_img.height-4; i++ )
-  for ( j = 4; j < input_img.width-4; j++ ) {
-    int total_red = 0;
-    int total_green = 0;
-    int total_blue  = 0;
-    for (int dx = -4; dx <= 4; dx++) {
-        for (int dy = -4; dy <= 4; dy++) {
-          total_red += input_img.color[0][i+dy][j+dx];
-          total_green += input_img.color[1][i+dy][j+dx];
-          total_blue += input_img.color[2][i+dy][j+dx];
+    unsigned char **seg = get_img(input_img.width, input_img.height, sizeof(unsigned char));
+    int numConPixels = 0;
+    double T = 2;
+    ConnectedSet({.m = 67, .n = 45}, T, input_img.mono, input_img.width, input_img.height, 1, seg, &numConPixels);
+    /* open final image file */
+    if ((fp = fopen("smoothed.tif", "wb")) == NULL)
+    {
+        fprintf(stderr, "cannot open file smoothed.tif\n");
+        exit(1);
+    }
+
+    /* write smoothed image */
+    if (write_TIFF(fp, &smoothed_image))
+    {
+        fprintf(stderr, "error writing TIFF file %s\n", argv[2]);
+        exit(1);
+    }
+
+    /* close smoothed image file */
+    fclose(fp);
+}
+
+void ConnectedNeighbors(
+    Pixel s,
+    double T,
+    unsigned char **img,
+    int width,
+    int height,
+    int *M,
+    Pixel c[4]
+)
+{
+    unsigned char s_value = img[s.m][s.n];
+    if (s.m != 0 && img[s.m-1][s.n] - s_value <= T) {
+        Pixel p = {.m = s.m-1, .n = s.n};
+        c[*M] = p;
+        (*M)++;
+    }
+    if (s.n != 0 && img[s.m][s.n-1] - s_value <= T) {
+        Pixel p = {.m = s.m, .n = s.n-1};
+        c[*M] = p;
+        (*M)++;
+    }
+    if (s.m != height-1 && img[s.m+1][s.n] - s_value <= T) {
+        Pixel p = {.m = s.m+1, .n = s.n};
+        c[*M] = p;
+        (*M)++;
+    }
+    if (s.n != width && img[s.m][s.n+1] - s_value <= T) {
+        Pixel p = {.m = s.m, .n = s.n+1};
+        c[*M] = p;
+        (*M)++;
+    }
+        
+}
+
+void ConnectedSet(
+    Pixel s,
+    double T,
+    unsigned char **img,
+    int width,
+    int height,
+    int ClassLabel,
+    unsigned int **seg,
+    int *NumConPixels
+)
+{
+    Pixel startPoints[width*height];
+    int index = 0;
+    startPoints[index] = s;
+    seg[s.m][s.n] = ClassLabel;
+    while (index != -1) {
+        s = startPoints[index--];
+        int *numNeighbors;
+        Pixel neighbors[4];
+        // Find the neighbors of the pixel s
+        ConnectedNeighbors(s, T, img, width, height, numNeighbors, neighbors);
+        for (int i = 0; i < numNeighbors; i++) {
+            Pixel p = neighbors[i];
+            // If we haven't marked this pixel as part of the set add it to the pixels to explore
+            if (seg[p.m][p.n] == 0) {
+                startPoints[++index] = p;
+                // Mark this pixel as part of the connected set
+                seg[s.m][s.n] = ClassLabel;
+                // Increment the number of connected pixels
+                (*NumConPixels)++;
+            }
         }
     }
-    smoothed_image.color[0][i][j] = total_red / 81;
-    smoothed_image.color[1][i][j] = total_green / 81;
-    smoothed_image.color[2][i][j] = total_blue / 81;
-  }
-
-  /* open final image file */
-  if ( ( fp = fopen ( "smoothed.tif", "wb" ) ) == NULL ) {
-      fprintf ( stderr, "cannot open file smoothed.tif\n");
-      exit ( 1 );
-  }
-    
-  /* write smoothed image */
-  if ( write_TIFF ( fp, &smoothed_image ) ) {
-      fprintf ( stderr, "error writing TIFF file %s\n", argv[2] );
-      exit ( 1 );
-  }
-    
-  /* close smoothed image file */
-  fclose ( fp );
 }
 
 void error(char *name)
 {
-    printf("usage:  %s  image.tiff \n\n",name);
+    printf("usage:  %s  image.tiff \n\n", name);
     printf("this program reads in a 24-bit color TIFF image.\n");
     printf("It then horizontally filters the green component, adds noise,\n");
     printf("and writes out the result as an 8-bit image\n");
