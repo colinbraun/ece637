2b3977355c11b2f80ae9d37ce6eb0ad7548b8f18
Colin Braun <electrolitic21@gmail.com>
2023-03-31 20:07:37 -0400

    Fix small issue in median filter

--- lab7/section2.c
+++ lab7/section2.c
@@ -51,17 +51,18 @@ int main(int argc, char **argv)
     
     // Flip colors of images
     Pair weights[25];
-    weights[0].n = 1; weights[1].n = 1; weights[2].n = 1; weights[3].n = 1; weights[4].n = 1; weights[5].n = 1; weights[6].n = 2;
-    weights[7].n = 2; weights[8].n = 2; weights[9].n = 1; weights[10].n = 1; weights[11].n = 2; weights[12].n = 2; weights[13].n = 1;
-    weights[14].n = 1; weights[15].n = 1; weights[16].n = 2; weights[17].n = 2; weights[18].n = 2; weights[19].n = 1; weights[20].n = 1;
-    weights[21].n = 1; weights[21].n = 1; weights[22].n = 1; weights[23].n = 1; weights[24].n = 1;
+    weights[0].n = 1; weights[1].n = 1; weights[2].n = 1; weights[3].n = 1; weights[4].n = 1;
+    weights[5].n = 1; weights[6].n = 2; weights[7].n = 2; weights[8].n = 2; weights[9].n = 1;
+    weights[10].n = 1; weights[11].n = 2; weights[12].n = 2; weights[13].n = 2; weights[14].n = 1;
+    weights[15].n = 1; weights[16].n = 2; weights[17].n = 2; weights[18].n = 2; weights[19].n = 1;
+    weights[20].n = 1; weights[21].n = 1; weights[22].n = 1; weights[23].n = 1; weights[24].n = 1;
     for (int row = 2; row < original_img.height-2; row++) {
         for (int col = 2; col < original_img.width-2; col++) {
             Pair window[25];
             for (int i = -2; i <= 2; i++) {
                 for (int j = -2; j <= 2; j++) {
-                    int index = (i+2)*(j+2) + j+2;
-                    window[index].m = img14sp_img.mono[row+i][col+i];
+                    int index = (i+2)*5 + j+2;
+                    window[index].m = img14gn_img.mono[row+i][col+j];
                     window[index].n = weights[index].n;
                 }
             }
@@ -73,7 +74,7 @@ int main(int argc, char **argv)
             result[row][col] = window[i].m;
         }
     }
-    save_grayscale_image_to_tiff_i(result, original_img.width, original_img.height, "2-img14sp-corrected.tif");
+    save_grayscale_image_to_tiff_i(result, original_img.width, original_img.height, "2-img14gn-corrected.tif");
 }
 
 // Sum all the weights between start and end in the array.
